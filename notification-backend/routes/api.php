c946e0eda8f72e4a8917f0532ddf0df31962e8e8
manoj010 <manojale100@gmail.com>
2025-06-04 23:38:07 +0545

    notification-backend

--- notification-backend/routes/api.php
+++ notification-backend/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\NotificationController;
-use App\Models\Notification;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::post('/notifications', [NotificationController::class, 'store']);
 Route::get('/notifications/recent', [NotificationController::class, 'recent']);
